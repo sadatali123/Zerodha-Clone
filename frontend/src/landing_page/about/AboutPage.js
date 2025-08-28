e261b9dc7ba4e25c2e237759a07a88591e9f569b
Sadat Ali <sa9621952@gmail.com>
2025-08-28 18:19:46 +0530

    add images and icon  link

---
+++ frontend/src/landing_page/about/AboutPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Hero from './Hero';
+import Team from './Team';
+import Footer from '../Footer';
+
+function AboutPage() {
+    return ( 
+        <>
+        <Navbar />
+        <Hero />
+        <Team />
+        <Footer />
+        </>
+     );
+}
+
+export default AboutPage;
