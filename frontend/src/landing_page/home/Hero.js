6e5d1ba00b1680b76b7bff5f0f08fdb4f7f06c16
Sadat Ali <sa9621952@gmail.com>
2025-08-31 05:09:45 +0530

    feat: create awards, pricing, stats pages

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
+import '../../index.css';
  function Hero() {
-    return ( <h1>Hero <i class="fa-solid fa-circle-user"></i></h1> );
+    return ( 
+         <div className="container p-5 mb-5">
+            <div className="row text-center">
+               <img src="/media/images/homeHero.png" alt="Hero Image" className="mb-5" />
+               <h1 className="mt-5">Invest in everything</h1>
+               <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+               <button type="button" className="btn btn-primary button">Sign up for free</button>
+            </div>
+         </div>
+
+     );
  }
  
  export default Hero;
--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -1,8 +1,63 @@
-import React from 'react';
-
+import React from "react";
+import "../../index.css";
 
 function Pricing() {
-    return ( <h1>Pricing</h1> );
+  return (
+    <div className="container">
+      <div className="row mx-5">
+        <div className="col-5">
+          <h2>Unbeatable pricing</h2>
+          <p className="text-muted mt-4">
+            We pioneered the concept of discount broking and price transparency
+            in India. Flat fees and no hidden charges.
+          </p>
+          <a href="" className="" style={{ textDecoration: "none" }}>
+            See Pricing <i class="fa-sharp fa-solid fa-arrow-right"></i>
+          </a>
+        </div>
+        <div className="col-7 ml-5 mt-3">
+          <div className="pricing-box">
+            <img src="media\images\pricing0.svg" alt="Pricing" />
+            <p>
+              Free account
+              <br></br>
+              opening
+            </p>
+          </div>
+          <div className="pricing-box">
+            <img src="media\images\pricing0.svg" alt="pricing" />
+            <p>
+              Free equity delivery
+              <br></br>
+              and direct mutual funds
+            </p>
+          </div>
+          <div className="pricing-box">
+            <img src="media\images\intradayTrades.svg" alt="pricing" />
+            <p>
+              Intraday and
+              <br></br>
+              F&O
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div>
+            <img src="media\images\education.svg" alt="Varsity" />
+          </div>
+        </div>
+        <div className="col-6">
+          <h2>Free and open market education</h2>
+          <p>
+            Varsity, the largest online stock market education book in the world
+            covering everything from the basics to advanced trading.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
