b46d24c4462102d22dfbc01d165f2340391ccb3b
Sadat Ali <sa9621952@gmail.com>
2025-09-03 03:38:05 +0530

    feat: add navbar

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
-    return ( <h1>Navbar</h1> );
+  return (
+      <nav class="navbar navbar-expand-lg border-bottom" style={{backgroundColor:"#fff",height:"70px"}}>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="media\images\logo.svg" alt="Logo" style={{width:"25%"}}/>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">
+                  Signup
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">
+                  Products
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Support
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  );
 }
 
 export default Navbar;
