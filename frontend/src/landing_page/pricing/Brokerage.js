f89c9b979812d0e44d6b2d7704a11cdec11c9d19
Sadat Ali <sa9621952@gmail.com>
2025-09-18 14:10:51 +0530

    update pricing section and add support section

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -73,7 +73,12 @@ function Brokerage() {
           </button>
         </li>
         <li className="nav-item fs-4">
-          <button className="nav-link disabled">Commodity</button>
+          <button 
+          className={`nav-link ${activeTab === "commodity" ? "active" : ""}`}
+           onClick={() => setActiveTab("commodity")}
+          >
+            Commodity
+          </button>
         </li>
       </ul>
 
@@ -211,6 +216,61 @@ function Brokerage() {
                 <td className="fw-bold text-start">Stamp charges</td>
                 <td>0.0001% or ₹10 / crore on buy side</td>
                 <td>0.0001% or ₹10 / crore on buy side</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+
+      {/* commodity table */}
+      {activeTab === "commodity" && (
+        <div className="table-responsive">
+          <table className="table table-bordered align-middle">
+            <thead className="table-light text-center">
+              <tr>
+                <th></th>
+                <th>Commodity futures</th>
+                <th>Commodity options</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="fw-bold text-start">Brokerage</td>
+                <td>0.03% or ₹20/executed order whichever is lower</td>
+                <td>₹20/executed order</td>
+              </tr>
+              <tr>
+                <td className="fw-bold text-start">STT/CTT</td>
+                <td>0.01% on sell side (Non-Agri)</td>
+                <td>0.05% on sell side</td>
+              </tr>
+              <tr>
+                <td className="fw-bold text-start">Transaction charges</td>
+                <td>
+                  MCX: 0.0021% <br /> NSE: 0.0001%
+                </td>
+                <td>
+                MCX: 0.0418% <br /> NSE: 0.001%
+                </td>
+              </tr>
+              <tr>
+                <td className="fw-bold text-start">GST</td>
+                <td>
+                  18% on (brokerage + SEBI charges + transaction charges)
+                </td>
+                <td>
+                  18% on (brokerage + SEBI charges + transaction charges)
+                </td>
+              </tr>
+              <tr>
+                <td className="fw-bold text-start">SEBI charges</td>
+                <td>Agri: <br/> ₹1 / crore <bt/> Non-agri: <br/> ₹10 / crore</td>
+                <td>₹10 / crore</td>
+              </tr>
+              <tr>
+                <td className="fw-bold text-start">Stamp charges</td>
+                <td>0.002% or ₹200 / crore on buy side</td>
+                <td>0.003% or ₹300 / crore on buy side</td>
               </tr>
             </tbody>
           </table>
--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,38 +1,46 @@
-import React from 'react';
+import React from "react";
+import "../../index.css";
 
+function Hero() {
+  return (
+    <div className="container mt-5 ">
+      <div className="row">
+        <h2 className="mb-4 fs-1 fw-semibold" style={{ marginLeft: "114px" }}>
+          Support Portal
+        </h2>
+        {/* <button className="btn btn-primary"
+        style={{width:"10%", marginLeft:"1060px"}}> My Tickets</button> */}
+      </div>
 
-function Hero(){
-    return(
-      <div className="container mt-5">
-
-      <div className=" row mb-4">
-        <h2 className="mb-4" style={{marginLeft:"114px"}}>Support Portal</h2>
-        <div className='col-1'></div>
+      <div className="row">
+        <div className="col-1"></div>
         <div className="col-10">
-            
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Eg: How do I open my account, How do I activate F&O..."
-        />
-
+          <input
+            style={{ height: "60px", width: "100%" }}
+            type="text"
+            className="form-control"
+            placeholder="Eg: How do I open my account, How do I activate F&O..."
+          />
         </div>
       </div>
 
       <div className="row mt-5">
         {/* Left Side Accordion */}
-        <div className='col-1'></div>
+        <div className="col-1"></div>
         <div className="col-6">
           <div className="accordion" id="supportAccordion">
+
             {/* Account Opening */}
             <div className="accordion-item">
-              <h2 className="accordion-header" id="headingOne">
+                <h2 className="accordion-header " id="headingOne">
                 <button
-                  className="accordion-button collapsed"
+                  className="accordion-button collapsed d-flex align-items-center justify-content-start gap-2 ps-1"
                   type="button"
                   data-bs-toggle="collapse"
                   data-bs-target="#collapseOne"
+                 
                 >
+                  <i class="fa-regular fa-square-plus text-primary fs-5"></i>
                   Account Opening
                 </button>
               </h2>
@@ -43,11 +51,21 @@ function Hero(){
               >
                 <div className="accordion-body">
                   <ul>
-                    <li><a href="#">Resident individual</a></li>
-                    <li><a href="#">Minor</a></li>
-                    <li><a href="#">Non Resident Indian (NRI)</a></li>
-                    <li><a href="#">Company, Partnership, HUF and LLP</a></li>
-                    <li><a href="#">Glossary</a></li>
+                    <li>
+                      <a href="#"  style={{lineHeight:"2.5"}}>Resident individual</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Minor</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Non Resident Indian (NRI)</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Company, Partnership, HUF and LLP</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Glossary</a>
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -55,36 +73,60 @@ function Hero(){
 
             {/* Zerodha Account */}
             <div className="accordion-item">
-              <h2 className="accordion-header" id="headingTwo">
+              <h2 className="accordion-header mt-3" id="headingTwo">
                 <button
-                  className="accordion-button collapsed"
+                  className="accordion-button collapsed d-flex align-items-center justify-content-start gap-2 ps-1 border-top"
                   type="button"
                   data-bs-toggle="collapse"
                   data-bs-target="#collapseTwo"
+                 
                 >
+                  <i className="fa-regular fa-circle-user fs-5  text-primary" ></i>
                   Your Zerodha Account
                 </button>
               </h2>
+
               <div
                 id="collapseTwo"
                 className="accordion-collapse collapse"
                 data-bs-parent="#supportAccordion"
               >
                 <div className="accordion-body">
-                  Content for Zerodha Account...
+                  <ul>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Your Profile</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Account modification</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>
+                        Client Master Report (CMR) and Depository Participant
+                        (DP)
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Nomination</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Transfer and conversion of securities</a>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
 
             {/* Kite */}
             <div className="accordion-item">
-              <h2 className="accordion-header" id="headingThree">
+                <h2 className="accordion-header mt-3" id="headingThree">
                 <button
-                  className="accordion-button collapsed"
+                  className="accordion-button collapsed d-flex align-items-center justify-content-start gap-2 ps-1 border-top"
                   type="button"
                   data-bs-toggle="collapse"
                   data-bs-target="#collapseThree"
+                 
                 >
+                  <i class="fa-regular fa-square-plus text-primary fs-5"></i>
                   Kite
                 </button>
               </h2>
@@ -94,12 +136,152 @@ function Hero(){
                 data-bs-parent="#supportAccordion"
               >
                 <div className="accordion-body">
-                  Content for Kite...
+                  <ul>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>IPO</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Trading FAQs</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Margin Trading Facility (MTF) and Margins</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Alerts snd Nudges</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>General</a>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
 
-            {/* Add more sections (Funds, Console, Coin) as needed */}
+            {/* Funds */}
+            <div className="accordion-item">
+                <h2 className="accordion-header mt-3" id="headingFour">
+                <button
+                  className="accordion-button collapsed d-flex align-items-center justify-content-start gap-2 ps-1 border-top"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseFour"
+                 
+                >
+                  <i class="fa-solid fa-indian-rupee-sign text-primary fs-5"></i>
+                  Funds
+                </button>
+              </h2>
+              <div
+                id="collapseFour"
+                className="accordion-collapse collapse"
+                data-bs-parent="#supportAccordion"
+              >
+                <div className="accordion-body">
+                  <ul>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Add money</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Withdraw money</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Add bank accounts</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>eMandates</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* console */}
+            <div className="accordion-item">
+                <h2 className="accordion-header mt-3" id="headingFive">
+                <button
+                  className="accordion-button collapsed d-flex align-items-center justify-content-start gap-2 ps-1 border-top"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseFive"
+                 
+                >
+                  <i class="fa-solid fa-circle-check text-primary fs-5"></i>
+                  Console
+                </button>
+              </h2>
+              <div
+                id="collapseFive"
+                className="accordion-collapse collapse"
+                data-bs-parent="#supportAccordion"
+              >
+                <div className="accordion-body">
+                  <ul>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Portfolio</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Corporate actions</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Funds statement</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Reports</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Profile</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Segments</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* coin */}
+            <div className="accordion-item">
+               <h2 className="accordion-header mt-3" id="headingSix">
+                <button
+                  className="accordion-button collapsed d-flex align-items-center justify-content-start gap-2 ps-1 border-top"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseSix"
+                 
+                >
+                  <i class="fa-solid fa-circle-dollar-to-slot text-primary fs-5"></i>
+                  Coin
+                </button>
+              </h2>
+              <div
+                id="collapseSix"
+                className="accordion-collapse collapse"
+                data-bs-parent="#supportAccordion"
+              >
+                <div className="accordion-body">
+                  <ul>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Mutual funds</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>National Pension Scheme (NPS)</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Fixed Deposit (FD)</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Features on Coin</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>Payments and Orders</a>
+                    </li>
+                    <li>
+                      <a href="#" style={{lineHeight:"2.5"}}>General</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -117,18 +299,26 @@ function Hero(){
           <div className="card">
             <div className="card-body">
               <h6>Quick links</h6>
-              <ul className="list-unstyled">
-                <li><a href="#">1. Track account opening</a></li>
-                <li><a href="#">2. Track segment activation</a></li>
-                <li><a href="#">3. Intraday margins</a></li>
-                <li><a href="#">4. Kite user manual</a></li>
+              <ul className="list-unstyled ">
+                <li>
+                  <a href="#" style={{lineHeight:"2.5"}}>1. Track account opening</a>
+                </li>
+                <li>
+                  <a href="#" style={{lineHeight:"2.5"}}>2. Track segment activation</a>
+                </li>
+                <li>
+                  <a href="#" style={{lineHeight:"2.5"}}>3. Intraday margins</a>
+                </li>
+                <li>
+                  <a href="#" style={{lineHeight:"2.5"}}>4. Kite user manual</a>
+                </li>
               </ul>
             </div>
           </div>
         </div>
       </div>
     </div>
-    );
+  );
 }
 
 export default Hero;
