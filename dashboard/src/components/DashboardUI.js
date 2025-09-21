be4023d6900850a95544d14a7844f1b20a43cdfa
Sadat Ali <sa9621952@gmail.com>
2025-09-21 23:05:33 +0530

    feat: add holdings, menu, summary and topbar components

--- dashboard/src/components/DashboardUI.js
+++ dashboard/src/components/DashboardUI.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 
 import Apps from "./Apps";
 import Funds from "./Funds";
@@ -8,15 +7,16 @@ import Holdings from "./Holdings";
 import Orders from "./Orders";
 import Positions from "./Positions";
 import Summary from "./Summary";
-import WatchList from "./WatchList";
-import { GeneralContextProvider } from "./GeneralContext";
+// import WatchList from "./WatchList";
+import { Routes, Route } from "react-router-dom";
+// import { GeneralContextProvider } from "../context/GeneralContext";
 
-const Dashboard = () => {
+const DashboardUI = () => {
   return (
     <div className="dashboard-container">
-      <GeneralContextProvider>
-        <WatchList />
-      </GeneralContextProvider>
+      {/* <GeneralContextProvider> */}
+        {/* <WatchList /> */}
+      {/* </GeneralContextProvider> */}
       <div className="content">
         <Routes>
           <Route exact path="/" element={<Summary />} />
@@ -31,4 +31,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default DashboardUI;
---
+++ dashboard/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import TopBar from "./Topbar";
+import Dashboard from "./DashboardUI";
+
+const Home = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+};
+
+export default Home;
