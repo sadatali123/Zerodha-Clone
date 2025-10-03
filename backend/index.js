41953d08cca7961d8d9a518fd5ebff415f65206e
Sadat Ali <sa9621952@gmail.com>
2025-10-04 01:23:38 +0530

    chore(server): setup base backend environment and configure MongoDB connection with Mongoose

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express =  require("express");
+const app = express();
+
+require("dotenv").config(); // helps to read .env file and added to process.env
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3002; // it is used to set the port number
+const uri = process.env.MONGO_URL ; // it is used to connect to mongoDB
+
+app.listen(PORT, () =>{
+    console.log("Server running on port 3002");
+    mongoose.connect(uri);
+    console.log("MongoDB connected");
+})
