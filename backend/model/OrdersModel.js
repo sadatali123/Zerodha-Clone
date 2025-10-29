72b13ea279d183e70ef5322bd10b2392e92c8eab
Sadat Ali <sa9621952@gmail.com>
2025-10-29 15:03:56 +0530

    feat(backend): implement backend API endpoints for holdings and positions

--- backend/model/OrdersModel.js
+++ backend/model/OrdersModel.js
@@ -2,4 +2,5 @@ const {OrdersSchema} = require("../schemas/OrdersSchema");
 const {model} = require("mongoose");
 
 const OrdersModel = model("orders", OrdersSchema);
+
 module.exports = {OrdersModel};     
