af37e2ac6f488669542e7a60c4d52950ad50f1f6
Sadat Ali <sa9621952@gmail.com>
2025-11-08 00:29:03 +0530

    Update signup page

--- dashboard/src/components/SignUp.js
+++ dashboard/src/components/SignUp.js
@@ -16,7 +16,6 @@ import axios from "axios";
 
 import { useAuth } from "../hooks/useAuth";
 import { Signup } from "../../../backend/controllers/AuthController";
-import "./signUp.css";
 const defaultTheme = createTheme();
 
 export default function Register() {
@@ -65,48 +64,49 @@ export default function Register() {
   };
 
   return (
-    <div className="form_container">
-      <h2>Signup Now</h2>
-      {alert.st && <Alert severity="error">{alert.msg}</Alert>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="Username">Username</label>
-          <input
-            type="text"
-            id="Username"
-            name="Username"
-            placeholder="Enter your username"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email Address</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter your email"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Enter your password"
-            required
-          />
-        </div>
-
-        <button type="submit">Sign Up</button>
-
-        <span>
-          Already have an account?{" "}
-          <Link to="/login">Sign in</Link>
-        </span>
-      </form>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Signup Now
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            {alert.st == true ? <Alert severity="error">{alert.msg}</Alert> : null}
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField autoComplete="given-name" name="Username" required fullWidth id="Username" label="Username" autoFocus />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField required fullWidth id="email" label="Email Address" name="email" autoComplete="email" />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField required fullWidth name="password" label="Password" type="password" id="password" autoComplete="new-password" />
+              </Grid>
+            </Grid>
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link to="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
