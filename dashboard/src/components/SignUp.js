a417ad0fae6f2ec0326bedcd24222ae335dae635
Sadat Ali <sa9621952@gmail.com>
2025-11-08 22:22:26 +0530

    Update styling of signup page

--- dashboard/src/components/SignUp.js
+++ dashboard/src/components/SignUp.js
@@ -64,7 +64,7 @@ export default function Register() {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
+  <ThemeProvider theme={defaultTheme}>
   <Container component="main" maxWidth="xs">
     <CssBaseline />
     <Box
